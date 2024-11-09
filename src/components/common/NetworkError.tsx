c1c8dd96d3aef68337aae1b7c45b04d247290c44
Ademola Onasoga <ademola.onasoga@gmail.com>
2024-11-09 11:11:19 +0100

    add call to action

--- src/components/common/NetworkError.tsx
+++ src/components/common/NetworkError.tsx
@@ -1,13 +1,13 @@
 import ErrorIcon from '../../assets/icons/error.svg';
 import Button from './Button';
 
-function NetworkError () {
+function NetworkError ({cta} : { cta: () => void }) {
   return (
     <div className="h-screen centre">
       <div className=''>
         <img src={ErrorIcon} alt="logo" />
         <p>Network Error</p>
-        <Button>Try again</Button>
+        <Button handleOnClick={cta}>Try again</Button>
 
       </div>
     </div>
