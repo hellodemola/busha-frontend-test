66ed6b06abf4b2d3bb8a1a3271a7e5519289be0c
Ademola Onasoga <ademola.onasoga@gmail.com>
2024-11-09 11:10:56 +0100

    update button styling

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,7 +2,7 @@ import { IButton } from "../../interface/IButton.interface";
 
 function Button ({children, handleOnClick}: IButton) {
  return (
-    <button onClick={handleOnClick}>
+    <button className="brand-button" onClick={handleOnClick}>
         {children}
     </button>
  )
