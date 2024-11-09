c7f01b525edb686a67f0c2a910daf691bf1458f0
Ademola Onasoga <ademola.onasoga@gmail.com>
2024-11-09 11:16:19 +0100

    add cta

--- src/components/forms/CreateWallet.forms.tsx
+++ src/components/forms/CreateWallet.forms.tsx
@@ -1,16 +1,22 @@
 import Loading from "../common/Loading";
 import NetworkToaster from "../common/Network";
 import NetworkError from "../common/NetworkError";
+import CancelIcon from '../../assets/icons/cancel.svg';
 
-function CreateWalletForm () {
+function CreateWalletForm ({onClose}: {onClose: () => void}) {
     return (
         <div style={{ padding: '2em' }}>
             <div>
             <Loading />
             </div>
-            <NetworkError />
+            <NetworkError cta={onClose} />
         <div>
+            <div style={{ display: "flex", justifyContent: 'space-between' }}>
             <h2>Add new wallet</h2>
+            <button onClick={onClose}>
+            <img src={CancelIcon} width="15em" alt="cancel" />
+            </button>
+            </div>
             <p>The crypto wallet will be created instantly and be available in your list of wallets.</p>
         <form>
             <select style={{ marginBottom: '2em', width: '100%', padding: '1em' }}>
